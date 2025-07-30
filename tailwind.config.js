bbe7fa26a4e92c8b6a2bcfeb0c4ed80e4909a324
felipecastillosm-max <felipecastillo.sm@gmail.com>
2025-07-30 14:01:03 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+// tailwind.config.js
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        loartune: {
+          verde: "#00BFA6",
+          grisClaro: "#F4F4F4",
+          grisOscuro: "#333333",
+          amarillo: "#FFC700",
+          coral: "#FF9F91",
+          rojo: "#FF5757",
+        },
+      },
+    },
   },
   plugins: [],
 };
